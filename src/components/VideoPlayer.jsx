8ef0700802c2e31b4545e6563361ce7f1ac6d6e1
MBintangS <muhamadbintang3012@gmail.com>
2024-11-08 00:51:15 +0700

    style videoplayer padding & border

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,9 +3,9 @@ import video from "/STUDY-IN-INDONESIA.mp4";
 
 const VideoPlayer = ({className}) => {
   return (
-    <div className={`max-w-6xl mx-auto w-full ${className} px-5 rounded-xl`}>
+    <div className={`max-w-6xl mx-auto w-full ${className} px-5 rounded-[20px]`}>
       <div className="bg-white p-2 rounded-xl">
-        <video src={video} controls className="rounded-2xl" />
+        <video src={video} controls className="rounded-xl" />
       </div>
     </div>
   );
