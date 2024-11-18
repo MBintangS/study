40900b749e8907ebe70d1a2d7372bf8cb20e1544
MBintangS <muhamadbintang3012@gmail.com>
2024-11-19 00:29:15 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react()]
+    plugins: [react()],
+    base: process.env.VITE_API_URL
   }
 })
