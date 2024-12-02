381c99cccda4a3a520f3f6c7bdf51c97af2d7ccc
MBintangS <muhamadbintang3012@gmail.com>
2024-12-02 16:07:05 +0700

    add placeholder video player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import video from "/STUDY-IN-INDONESIA.mp4";
 
-const VideoPlayer = ({className}) => {
+const Placeholder = () => (
+  <div className="w-full min-h-[170px] md:h-[350px] xl:h-[550px] flex items-center justify-center bg-gray-200 rounded-xl">
+    <div className="loader border-t-transparent border-blue-500"></div>
+  </div>
+);
+
+const VideoPlayer = ({ className }) => {
+  const [isVideoReady, setIsVideoReady] = useState(false);
+
+  // Handler ketika video siap
+  const handleVideoLoaded = () => {
+    
+    setTimeout(() => {
+      setIsVideoReady(true);
+    }, 1000);
+  };
+
   return (
-    <div className={`max-w-6xl mx-auto w-full ${className} px-5 rounded-[20px]`}>
+    <div
+      className={`max-w-6xl mx-auto w-full ${className} px-5 rounded-[20px]`}
+    >
       <div className="bg-white p-2 rounded-xl">
-        <video src={video} controls className="rounded-xl" />
+        {!isVideoReady && (
+          <div className="w-full h-full flex items-center justify-center bg-gray-200 rounded-xl">
+            <Placeholder />
+          </div>
+        )}
+        <video
+          src={video}
+          controls
+          className={`rounded-xl ${!isVideoReady ? "hidden" : "block"}`}
+          onLoadedData={handleVideoLoaded}
+        />
       </div>
     </div>
   );
